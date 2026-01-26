9fafdbaf48a9da50b72dd59d7e3d6fd0812fe47f
thenameisvicky <vigneshbalasubramaniyan3@gmail.com>
2026-01-26 17:12:18 +0530

    [FEAT] - Build configuration and Queue system implementation

--- application/queue/server.ts
+++ application/queue/server.ts
@@ -1,5 +1,27 @@
+import { Worker } from 'bullmq';
+
 /**
  * BullMQ server (Consumer)
  * Consumes from Producer
  * Handles Inference
  */
+
+const worker = new Worker("LLM_INFERENCE", async job => {
+    console.log(`Consuming...Job-${job.id} Payload-${JSON.stringify(job.data)}`);
+
+    await new Promise(r => setTimeout(r, 1000));
+    console.log("Job completed:", job.id);
+}, {
+    connection: {
+        host: '127.0.0.1',
+        port: 6379
+    }
+});
+
+worker.on("ready", () => {
+    console.log("Consumer ready!");
+});
+
+worker.on("failed", () => {
+    console.log("Consumer crashed!")
+})
--- application/server.ts
+++ application/server.ts
@@ -1,5 +1,42 @@
+import express from 'express';
+import { Queue } from 'bullmq';
+
 /**
  * Express server (MAIN)
  * Receives API request - Enqueues it
  * Producer for Queue
  */
+
+const app = express();
+app.use(express.json());
+
+const queue = new Queue('LLM_INFERENCE', {
+    connection: {
+        host: '127.0.0.1',
+        port: 6379,
+    }
+})
+
+
+/**
+ * POST /api/v1/chat
+ * Enqueues the user query
+ * Queue - LLM_INFERENCE
+ * Action - TEXT_GENERATION
+ */
+
+app.post('/api/v1/chat', async (req, res) => {
+    const { message } = req.body;
+
+    const job = await queue.add("TEXT_GENERATION", {
+        message: message
+    });
+
+    return res.json({
+        message: `Request enqueued successfully - id: ${job.id}, data: ${JSON.stringify(job.data)}`
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Application Listening on port 3000");
+})
